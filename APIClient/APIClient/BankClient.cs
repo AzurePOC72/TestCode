3cc05270e3fde242f582271e86a6c6faea0b726b
AzurePOC72 <63785932+AzurePOC72@users.noreply.github.com>
2020-04-29 21:20:00 +0530

    Sample code

---
+++ APIClient/APIClient/BankClient.cs
@@ -0,0 +1,51 @@
+﻿using APIClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClient
+{
+    public partial class ApiClient
+    {
+        public async Task<List<Bank>> GetBanks()
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "api/Bank"));
+            return await GetAsync<List<Bank>>(requestUrl);
+        }
+
+        public async Task<Bank> GetBanks(int Id)
+        {
+            string queryString = $"/{Id}";
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"api/Bank{queryString}"));
+            return await GetAsync<Bank>(requestUrl);
+        }
+
+        public async Task<Message<Bank>> SaveBank(Bank model)
+        {
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "api/Bank"));
+            return await PostAsync<Bank>(requestUrl, model);
+        }
+
+        public async Task<HttpResponseMessage> UpdateBank(int Id, Bank model)
+        {
+            string queryString = $"/{Id}";
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"api/Bank{queryString}"));
+            return await PutAsync<Bank>(requestUrl, model);
+        }
+        
+        public async Task<HttpResponseMessage> DeleteBank(int Id)
+        {
+            string queryString = $"/{Id}";
+            var requestUrl = CreateRequestUri(string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                $"api/Bank{queryString}"));
+            return await DeleteAsync(requestUrl);
+        }
+    }
+}
---
+++ APIClient/APIClient/Models/BankModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClient.Models
+{
+    [DataContract]
+    public class Bank
+    {
+        [DataMember]
+        public int BankID { get; set; }
+        [DataMember]
+        public string BankName { get; set; }
+    }
+}
